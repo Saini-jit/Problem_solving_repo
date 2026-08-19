6e23b1b6984327e3973f92ba3a9917e401dab7bf
Saini-jit <sainijitendra75700@gmail.com>
2026-08-19 22:50:57 +0530

    Work on stack is started

--- STACK_intro_._repo/Stack_intro.java
+++ STACK_intro_._repo/Stack_intro.java
@@ -1,6 +1,25 @@
 public class Stack_intro{
+
     public static void main(String[] args) {
         System.out.println("hellow");
-        // stack follows the FILO 
+        // 1.stack follows the FILO principle means First in last out 
+        // 2. in stack push(instation) pop(dletion) operations or done one the top element of the stack
+        // 3. stack's are of 2 type static and dynamic
+
+        //  static stack
+        //      1. static stack is created by array and
+        //          there is a problem of over flow (means trying to insert a element in a full stack).
+        //          and under flow (means trying to remove element     from impty stack)
+
+        //  dinamic stack
+        //      1. dynamic stack cand be made by linkedlist and
+        //      2. it can also made by vctor in c++ and arraylist in java.
+        //      3. there is no problem of overflow and underflow
+
+        //making stack using array
+
+        
+        
+
     }
 }

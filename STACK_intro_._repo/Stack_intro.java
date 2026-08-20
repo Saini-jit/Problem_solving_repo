b30f9ac2f97f373b84ffddf987917cdd02b688c4
Jitendra Saini <sainijitendra75700@gmail.com>
2026-08-20 12:10:34 +0530

    stack functions implemened

--- STACK_intro_._repo/Stack_intro.java
+++ STACK_intro_._repo/Stack_intro.java
@@ -1,7 +1,36 @@
 public class Stack_intro{
-
+    private int[] arr;
+    private int capicity;
+    private int top;
+    Stack_intro(int cap)
+    {
+        capicity=cap;
+        arr=new int[capicity];
+        top=-1;
+    }
+    public void push(int x)
+    {
+        if(top==(capicity-1)){
+        System.out.println("stack Overflow");
+        }
+        else{
+        ++top;
+        arr[top]=x;
+    }}
+    
+    public int pop(){
+        if(top==(-1))
+        {
+            System.out.println("Stack Underflow");
+            return top;
+        }
+        else{
+            return arr[top];
+            top--;
+        }
+    }
+        
     public static void main(String[] args) {
-        System.out.println("hellow");
         // 1.stack follows the FILO principle means First in last out 
         // 2. in stack push(instation) pop(dletion) operations or done one the top element of the stack
         // 3. stack's are of 2 type static and dynamic
